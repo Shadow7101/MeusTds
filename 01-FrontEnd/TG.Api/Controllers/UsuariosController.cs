37c115dcdb5452c68a0f2b77611d0df53d2e38b1
WILSON <shadow7001@outlook.com>
2019-12-30 02:36:19 -0300

    depois de aberto no visual studio

--- 01-FrontEnd/TG.Api/Controllers/UsuariosController.cs
+++ 01-FrontEnd/TG.Api/Controllers/UsuariosController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TD.Dominio.Aplicacao;
 using TD.Dominio.ViewModel;
 
 namespace TG.Api.Controllers
@@ -12,10 +13,26 @@ namespace TG.Api.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        public IUsuarios Usuarios { get; }
+
+        public UsuariosController(IUsuarios usuarios)
+        {
+            Usuarios = usuarios;
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Post(Login model)
         {
-
+            try
+            {
+                var usuario = await Usuarios.Autenticacao(model);
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
     }
--- 01-FrontEnd/TG.Api/Startup.cs
+++ 01-FrontEnd/TG.Api/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TD.Aplicacao;
+using TD.Dominio.Aplicacao;
 
 namespace TG.Api
 {
@@ -26,6 +28,7 @@ namespace TG.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IUsuarios, Usuarios>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
